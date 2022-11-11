3b85d5acf8599a1572c0acb87d793a772bc6b4be
Lucas Walker <yugiohmades@gmail.com>
2022-11-11 14:40:50 -0300

    .

---
+++ quarto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hotel.c"
+#include "quarto.h"
+#include "hotel.h"
+
+
+struct quarto {
+    char tipo[100];
+    float preco;
+    char avaliacao[100];
+};
+
